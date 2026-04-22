01d1def2d1668e5f0d81f197930bdf827a0a4f77
manishtomardce <manishtomarpsu@gmail.com>
2026-04-23 02:02:49 +0530

    Update PetGo listing Discover & Book 4 ...

--- app/page.tsx
+++ app/page.tsx
@@ -414,7 +414,7 @@ export default function HomePage() {
                 </h1>
 
                 <div className="mt-3 text-center">
-                  <p className="text-[14px]  text-[#2E2A26]">
+                  <p className="text-[14px]  text-[#2d2b28]">
                     Discover & Book the perfect club for your dog
                   </p>
                  
